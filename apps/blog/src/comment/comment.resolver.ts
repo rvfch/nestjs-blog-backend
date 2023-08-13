a93403156554cac5526b1a688365d043ad7e198d
Roman Filatov <rvfchdev@gmail.com>
2023-08-13 20:39:14 +0200

    Add docs

--- apps/blog/src/comment/comment.resolver.ts
+++ apps/blog/src/comment/comment.resolver.ts
@@ -16,11 +16,22 @@ import { TenantGqlInterceptor } from './interceptors/tenant-gql.interceptor';
 export class CommentResolver {
   constructor(private readonly commentService: CommentService) {}
 
+  /**
+   * Create a new comment.
+   * @param {CreateCommentDto} dto - Data Transfer Object for creating a comment.
+   * @returns {CommentDto} The created comment.
+   */
   @Mutation(() => CommentDto)
   public async createComment(@Args('dto') dto: CreateCommentDto) {
     return await this.commentService.createComment(dto);
   }
 
+  /**
+   * Get all comments associated with a given article.
+   * @param {string} articleId - ID of the article.
+   * @param {string} userId - ID of the current user.
+   * @returns {CommentDto[]} List of comments for the article.
+   */
   @Public()
   @Query(() => [CommentDto])
   public async getComments(
@@ -30,6 +41,11 @@ export class CommentResolver {
     return await this.commentService.getCommentsByArticleId(articleId, userId);
   }
 
+  /**
+   * Rate a given comment.
+   * @param {RateCommentDto} dto - Data Transfer Object for rating a comment.
+   * @returns {CommentDto} The rated comment.
+   */
   @Mutation(() => CommentDto)
   public async rateComment(
     @Args('dto') dto: RateCommentDto,
@@ -37,12 +53,20 @@ export class CommentResolver {
     return await this.commentService.rateComment(dto);
   }
 
+  /**
+   * Subscription for comment creation events.
+   * @returns {AsyncIterator} Asynchronous iterator for comment creation events.
+   */
   @Public()
   @Subscription(() => CommentDto)
   commentCreated() {
     return this.commentService.pubSub.asyncIterator('commentCreated');
   }
 
+  /**
+   * Subscription for comment rating events.
+   * @returns {AsyncIterator} Asynchronous iterator for comment rating events.
+   */
   @Public()
   @Subscription(() => CommentDto)
   commentRated() {
